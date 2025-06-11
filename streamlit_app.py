f8a2dd196ab2e5686365a31e1ced635e4e95af4e
JKSychta <83690016+JKSychta@users.noreply.github.com>
2025-06-11 20:56:33 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,14 @@ st.write(
 # Ask user for their OpenAI API key via `st.text_input`.
 # Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
 # via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
-openai_api_key = st.text_input("OpenAI API Key", type="password")
+# openai_api_key = st.text_input("OpenAI API Key", type="password")
+openai_api_key = st.secrets["API_KEY"]
+
+
+api_key, base_url = st.secrets["API_KEY"], st.secrets["BASE_URL"]
+selected_model = "google/gemma-3-1b-it:free"
+model = chat_openrouter.ChatOpenRouter(model_name=selected_model)
+
 if not openai_api_key:
     st.info("Please add your OpenAI API key to continue.", icon="🗝️")
 else:
